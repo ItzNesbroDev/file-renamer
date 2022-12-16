9776d75cf114f823e16f0f8cb452c8c0539fd26d
ItzNesbroDev <asnesbeer3@gmail.com>
2022-12-16 09:47:27 +0300

    chore(main.rs): formtted

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,21 @@ fn main() -> Result<(), Box<dyn Error>> {
         .get_matches();
 
     // Read directory and rename files
-let directory = matches.value_of("directory").unwrap();
-let rule = matches.value_of("rule").unwrap();
-for entry in fs::read_dir(directory)? {
-    let old_path = entry?.path();
-    let old_name = old_path.file_name().unwrap().to_str().unwrap();
-    let (name, extension) = old_name.split_at(old_name.rfind('.').unwrap_or_else(|| old_name.len()));
-    let new_name = match rule {
-        "uppercase" => name.to_uppercase(),
-        "lowercase" => name.to_lowercase(),
-        _ => name.to_owned(),
-    };
-    let new_path = old_path.with_file_name(format!("{}{}", new_name, extension));
-    fs::rename(old_path, new_path)?;
-}
-
+    let directory = matches.value_of("directory").unwrap();
+    let rule = matches.value_of("rule").unwrap();
+    for entry in fs::read_dir(directory)? {
+        let old_path = entry?.path();
+        let old_name = old_path.file_name().unwrap().to_str().unwrap();
+        let (name, extension) =
+            old_name.split_at(old_name.rfind('.').unwrap_or_else(|| old_name.len()));
+        let new_name = match rule {
+            "uppercase" => name.to_uppercase(),
+            "lowercase" => name.to_lowercase(),
+            _ => name.to_owned(),
+        };
+        let new_path = old_path.with_file_name(format!("{}{}", new_name, extension));
+        fs::rename(old_path, new_path)?;
+    }
 
     Ok(())
 }
-
